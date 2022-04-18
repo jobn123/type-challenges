ab6d2ca34c857c43ef45795231f54a4235417f7a
jobn123 <jobn_joy@163.com>
2022-04-18 10:07:51 +0800

    feat: add medium promise-all

---
+++ questions/20-medium-promise-all/template.ts
@@ -0,0 +1,3 @@
+// declare function PromiseAll (values: any): any
+
+declare function PromiseAll<T extends unknown[]> (values: readonly [...T]): Promise<{ [P in keyof T]: T[P] extends Promise<infer R> ? R : T[P] }>
