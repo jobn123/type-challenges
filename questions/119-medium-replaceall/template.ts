2269d16df1a9ab8d27a8105b48f8c9a93c7f1cdd
jobn123 <jobn_joy@163.com>
2022-04-23 20:38:20 +0800

    feat: add medium replaceAll

---
+++ questions/119-medium-replaceall/template.ts
@@ -0,0 +1,9 @@
+type ReplaceAll<
+  S extends string,
+  From extends string,
+  To extends string
+  > = From extends ''
+  ? S
+  : S extends `${infer X}${From}${infer Y}`
+  ? `${ReplaceAll<X, From, To>}${To}${ReplaceAll<Y, From, To>}`
+  : S
